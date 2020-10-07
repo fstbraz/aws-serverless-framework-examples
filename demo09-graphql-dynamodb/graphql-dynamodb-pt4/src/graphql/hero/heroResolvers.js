f897ce680d532a6994133799f569bac065c841db
Fausto Braz <fausto@Faustos-MBP.lan>
2020-10-07 23:29:55 +0100

    graphql changes

---
+++ demo09-graphql-dynamodb/graphql-dynamodb-pt4/src/graphql/hero/heroResolvers.js
@@ -0,0 +1,27 @@
+const resolvers = {
+    Hero: {
+        // for each hero getHero returns, this function will be executed like a subquery 
+        async skills(root, args, context){
+            const skills = root.skills.map(el => context.Skill.findOne(el))
+            const results = await Promise.all(skills)
+            const all = results.reduce(prev)
+            console.log(root)
+            return [];
+        }
+    },
+    // GET
+    Query: {
+        async getHero(root, args, context, info){
+            return context.Hero.findAll(args)
+        }
+    },
+    // POST (graphql doesnt use PUT)
+    Mutation: {
+         async createHero(root, args, context, info){
+            const { id } = await context.Hero.create(args)
+            return id
+        }
+    }
+}
+
+module.exports = resolvers
